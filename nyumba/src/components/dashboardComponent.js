e4f509211d7abb6725af7576ded648cecdb58d29
Amos Welike <amoswelike@gmail.com>
2019-06-12 20:10:05 +0300

    add dasboard view and component

---
+++ nyumba/src/components/dashboardComponent.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import dashboardView from '../views/dashboardView';
+
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return (
+        <div>
+        <dashboardView />
+        </div> );
+    }
+}
+ 
+export default Dashboard;
---
+++ nyumba/src/views/dashboardView.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+const dashboardView = ({}) => {
+    return (
+    <div>
+        Welcome to the dasboard
+    </div>
+)};
+
+export default dashboardView;
